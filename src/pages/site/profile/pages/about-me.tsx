ea907d994dd0e89929355bab6e3d93993f20dc81
neominal.solutions <neominal.solutions@gmail.com>
2025-01-30 10:24:19 +0300

    User Profile About Me

--- src/pages/site/profile/pages/about-me.tsx
+++ src/pages/site/profile/pages/about-me.tsx
@@ -1,5 +1,14 @@
+import { useParams } from 'react-router-dom';
+
 function AboutMe() {
-	return <></>;
+	// useParams hook'u ile url'den parametre alınabilir
+	const params = useParams();
+
+	const username = params.username;
+	console.log(username);
+
+	// username nasıl alınır?
+	return <>Kullanıcı Adı: {username}</>;
 }
 
 export default AboutMe;
